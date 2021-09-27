5e0b5506b7c5da93240ffa4f4f0510d4cb190932
imxnat <ngrm97@gmail.com>
2021-09-26 21:37:59 -0400

    :fire: remove code

--- frontend/src/pages/Homepage.js
+++ frontend/src/pages/Homepage.js
@@ -1,23 +1,24 @@
 import { Link } from 'react-router-dom'
 import useFetch from '../hooks/useFetch'
 
-
 const Homepage = () => {
+
     //destructuring
     const { loading, data, error } = useFetch('http://localhost:1337/reviews')
 
-    if(loading) return <div className="lds-facebook"><div></div><div></div><div></div></div>
+    if(loading) return  <div className="lds-facebook"><div></div><div></div><div></div></div>
     if(error) return <p>Something went wrong :(</p>
-
+    
     return (
         <div>
             {/* // LIST OF REVIEWS */}
            {data.map(review => (
             <div key={review.id} className='review-card'>
-              <div className="rating" >{review.rating}</div>
-              <h2>{review.title}</h2>
-
-              <small>console list</small>
+            <div className="rating"> {review.rating}</div>
+            
+              <h2>{review.title} </h2>
+            
+              <small>Created: {review.created_at.substring(0,10)}  |  Last Update: {review.updated_at.substring(0,10)}</small>
 
               <p>{review.body.substring(0, 200)}</p>
               <Link to={`/details/${review.id}`}>Read more</Link>
